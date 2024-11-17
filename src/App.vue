6a0808b46e01e054a156285a8a5d52544f3588bc
Aran <1316599329@qq.com>
2024-11-17 23:08:29 +0800

    mockjs使用学习demo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" />
+     -->
+    <el-button @click="add">测试添加</el-button>
+    <el-table
+      :data="tableData"
+      :default-sort="{ prop: 'id', order: 'descending' }"
+      ref="table"
+    >
+      <el-table-column prop="id" sortable label="id" width="180">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table-column prop="date" label="日期"> </el-table-column >
+       <el-table-column label="操作"   fixed="right">  <template scope="{row}"> 
+        <div>
+           <el-button @click="dataDelete(row.id)">删除</el-button>
+           <el-button @click="edit(row.id)">修改</el-button>
+        </div>
+      </template></el-table-column>
+     
+    </el-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from "./components/HelloWorld.vue";
+import Mock from "mockjs";
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  // components: {
+  //   HelloWorld,
+  // },
+  data() {
+    return {
+      tableData: [],
+    };
+  },
+  created() {
+    this.mockData();
+    this.getData();
+  },
+  methods: {
+    mockData() {
+      Mock.mock("/api/user", "get", {
+        code: 200,
+        "data|55": [
+          {
+            id: "@increment",
+            name: "@cname()",
+            address: "@city(true)",
+            date: "@date(yyyy-MM-dd)",
+          },
+        ],
+      });
+      Mock.mock(RegExp("/api/user"), "post", (req) => {
+        const params = JSON.parse(req.body);
+        this.tableData.unshift(
+          Mock.mock({
+            id: "@increment",
+            name: params.name,
+            address: params.address,
+            date: params.date,
+          })
+        );
+      });
+      Mock.mock(RegExp("/api/user"), "delete", (req) => {
+        const params = JSON.parse(req.body);
+        const deleteIndex = this.tableData.findIndex((item) => {
+          return item.id === params;
+        });
+        this.tableData.splice(deleteIndex, 1);
+
+        return {
+          code: 200,
+          success: true,
+          message: "用户删除成功",
+        };
+      });
+    },
+    add() {
+      const params = {
+        name: "测试",
+        address: "测试地址",
+        date: "2004-1-1",
+      };
+      axios.post("/api/user", params).then((res) => {
+        console.log(res);
+      });
+    },
+    dataDelete(params) {
+      axios.delete("/api/user", params).then((res) => {
+           console.log(res);
+      });
+    },
+    edit(){
+      axios.updata("/api/user", params).then((res) => {
+           console.log(res);
+      });
+    },
+    getData() {
+      axios.get("/api/user").then((res) => {
+        console.log(res);
+        this.tableData = res.data.data;
+      });
+    },
+  },
+};
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,39 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="debounceFn()">防抖</button>
+    <button @click="throttleFn()">节流</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      debounceTimer: null,
+      throttleTimer: null,
+    };
+  },
+  methods: {
+    // 防抖函数
+    debounceFn() {
+      clearTimeout(this.debounceTimer);  // 清除之前的定时器
+      this.debounceTimer = setTimeout(() => {
+        console.log("防抖触发：123");
+      }, 2000);
+    },
+
+    // 节流函数
+    throttleFn() {
+      if (this.throttleTimer) return;  // 如果定时器存在，直接返回
+      this.throttleTimer = setTimeout(() => {
+        console.log('节流触发');
+        this.throttleTimer = null;  // 释放定时器
+      }, 200);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+/* 添加样式（如果需要） */
 </style>
